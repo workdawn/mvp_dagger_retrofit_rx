4456cbcf16b31b002eddee6fffc82ce928f7ec84
2008miaowei@163.com <miaowei1890312>
2016-05-05 10:44:06 +0800

    添加相关注释

--- app/src/main/java/demo/zhh/com/mvp_retrofit_dragger2_rx/utils/DisplayImageListenerAdapter.java
+++ app/src/main/java/demo/zhh/com/mvp_retrofit_dragger2_rx/utils/DisplayImageListenerAdapter.java
@@ -11,21 +11,22 @@ import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
  * Created by admin on 2016/5/5.
  */
 public abstract class DisplayImageListenerAdapter implements ImageLoadingListener{
+    /**图片加载开始*/
     @Override
     public void onLoadingStarted(String s, View view) {
 
     }
-
+    /**图片加载失败*/
     @Override
     public void onLoadingFailed(String s, View view, FailReason failReason) {
 
     }
-
+    /**图片加载结束*/
     @Override
     public void onLoadingComplete(String s, View view, Bitmap bitmap) {
 
     }
-
+    /**图片加载取消*/
     @Override
     public void onLoadingCancelled(String s, View view) {
 
