de2f97ed88991f489e2117110cb8a05103ee4147
2008miaowei@163.com <miaowei1890312>
2016-05-13 09:49:45 +0800

    添加rx线程切换

--- app/src/main/java/demo/zhh/com/mvp_retrofit_dragger2_rx/model/impl/MainModelImpl.java
+++ app/src/main/java/demo/zhh/com/mvp_retrofit_dragger2_rx/model/impl/MainModelImpl.java
@@ -7,6 +7,7 @@ import demo.zhh.com.mvp_retrofit_dragger2_rx.model.BaseModel;
 import demo.zhh.com.mvp_retrofit_dragger2_rx.model.MainModel;
 import rx.Observer;
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 /**
@@ -16,7 +17,7 @@ import rx.schedulers.Schedulers;
 public class MainModelImpl  extends BaseModel implements MainModel {
     @Override
     public Subscription loadDataFromService(final RequestResult<PaperInfo> result) {
-        return service.getPaper(1, "Xiaomi%28MI%204LTE%29", "05014648279f94ff80efc3655e09a6b4", 1, "1001", "60727212", 102, 0, 1, 1080, 1920, 2160, 1920, 76, "zh_CN", "", 0, 1, 3).subscribeOn(Schedulers.io()).subscribe(new Observer<PaperInfo>() {
+        return service.getPaper(1, "Xiaomi%28MI%204LTE%29", "05014648279f94ff80efc3655e09a6b4", 1, "1001", "60727212", 102, 0, 1, 1080, 1920, 2160, 1920, 76, "zh_CN", "", 0, 1, 3).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<PaperInfo>() {
             @Override
             public void onCompleted() {
 
